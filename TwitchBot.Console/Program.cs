246acc09b5fd6b74b0b820fc48ed6d0a74d3efca
Gabriel Santos <gabe.santos1@outlook.com>
2020-08-05 22:33:44 -0300

    bot anti bot

---
+++ TwitchBot.Console/Program.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+using TwitchBot.Application.Services;
+using TwitchBot.Services.TwitchService;
+
+namespace TwitchBot.ConsoleApp
+{
+    partial class Program
+    {
+        public static string channel = "aazacs";
+
+        static async Task Main(string[] args)
+        {
+            ServiceProvider = ConfigureCollection();
+            ConfigureProviders();
+            ConfigureMessageHandlers();
+
+            string username, password;
+
+            Console.WriteLine("Twitch username:");
+            username = Console.ReadLine();
+
+            Console.WriteLine("Twitch OAUTH password:");
+            password = Console.ReadLine();
+
+            Console.WriteLine("Channel to connect:");
+            channel = Console.ReadLine();
+
+            twitchService.Username = username;
+            twitchService.Password = password;
+
+            await twitchService.ConnectAsync(channel);
+            Console.WriteLine("[*] Awaiting connection...");
+            while (true)
+            {
+                await twitchService.ReadMessagesAsync(channel);
+            }
+        }
+
+        private static bool IsBot(string username)
+        {
+            if (username[0].Equals('@'))
+                username = username.Substring(1, username.Length-1);
+                
+            string guidValue = username.PadRight(32, '0');
+            var isBot = Guid.TryParseExact(guidValue, "N", out _);
+            if (isBot)
+                Console.WriteLine("ban");
+            else
+                Console.WriteLine("safe");
+            return isBot;
+        }
+
+        private static void Service_MessageReceived(object sender, MessageReceivedEventArgs e)
+        {
+            if (e.IsUserMessage)
+            {
+                Console.WriteLine($"[*] {e.UsrMessage.User}: {e.UsrMessage.Message}");
+                if (IsBot(e.UsrMessage.User))
+                {
+                    Task.Run(async () => await twitchService.SendMessageAsync($"/ban {e.UsrMessage.User} BOT", channel));
+                }
+            }
+            else
+            {
+                Console.WriteLine($"[*] {e.ToString()}");
+            }
+
+            if (e.FromSystemFormat.Contains("366", StringComparison.OrdinalIgnoreCase))
+            {
+                connected = true;
+                Console.WriteLine("[*] Connected");
+            }
+        }
+    }
+}
---
+++ TwitchBot.Console/ProgramConfigurations.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TwitchBot.Application.Services;
+using TwitchBot.Services.TwitchService;
+
+namespace TwitchBot.ConsoleApp
+{
+    partial class Program
+    {
+        public static bool connected = false;
+        private static ITwitchService twitchService;
+
+        private static IMessageHandler messageHandler;
+        private static ServiceProvider ServiceProvider { get; set; }
+
+        public static ServiceProvider ConfigureCollection()
+        {
+            var collection = new ServiceCollection();
+
+            collection.AddScoped<ITwitchService, TwitchService>();
+            collection.AddScoped<IMessageHandler, MessageHandler>();
+
+            collection.BuildServiceProvider();
+            return collection.BuildServiceProvider();
+        }
+
+        public static void ConfigureMessageHandlers()
+        {
+            twitchService.MessageReceived += messageHandler.PongMessageReceivedHandler;
+            twitchService.MessageReceived += Service_MessageReceived;
+        }
+
+        public static void ConfigureProviders()
+        {
+            twitchService = ServiceProvider.GetService<ITwitchService>();
+            messageHandler = ServiceProvider.GetService<IMessageHandler>();
+        }
+    }
+}
